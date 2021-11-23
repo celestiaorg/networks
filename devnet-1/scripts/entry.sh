b974eb05e391c2f7fcd71c024d33f3ee3fe25ae4
Nhu Viet Nguyen <braveryandglory@gmail.com>
2021-11-23 03:21:09 +0100

    scripts: create helper scripts to speed up devnet syncro between participants
    migrating outdated entry.sh to devnet-1 directory

--- devnet-1/scripts/entry.sh
+++ devnet-1/scripts/entry.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Variables with upper letters only are set in Dockerfile
 # Set variables
 capp=celestia-appd
 monk="james"
@@ -19,7 +18,7 @@ node_addr=$($capp keys show $NODE_NAME -a --keyring-backend $KEY_TYPE)
 
 $capp add-genesis-account $node_addr $COINS_TYPE --keyring-backend $KEY_TYPE
 
-$capp gentx $NODE_NAME 5000000000celes --keyring-backend=$KEY_TYPE --chain-id $CHAIN_ID
+#$capp gentx $NODE_NAME 5000000000celes --keyring-backend=$KEY_TYPE --chain-id $CHAIN_ID
 #$capp collect-gentxs
 
 # Set proper defaults and change ports
@@ -29,3 +28,6 @@ sed -i 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.celestia-app/confi
 sed -i 's/index_all_keys = false/index_all_keys = true/g' ~/.celestia-app/config/config.toml
 # Open up rest api
 sed -i '104 s/enable = false/enable = true/' ~/.celestia-app/config/app.toml
+
+# Uncomment line below, if you want to start the app right after initialisation
+# $capp start
